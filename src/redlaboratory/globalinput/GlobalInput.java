0171f0524efff0aa57f9ab7be58b86d92aab7362
RedLaboratory <rlj1202@gmail.com>
2016-08-02 21:39:47 +0900

    First upload

---
+++ src/redlaboratory/globalinput/GlobalInput.java
@@ -0,0 +1,66 @@
+package redlaboratory.globalinput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GlobalInput {
+	
+	private static boolean libraryLoaded = false;
+	
+	static {
+		libraryInitialize();
+	}
+	
+	public static native void typeString(String str);	
+	
+	private static void libraryInitialize() {
+		if (libraryLoaded) return;
+		
+		libraryLoaded = true;
+		
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+			if (System.getProperty("os.arch").contains("64")) {
+				loadLibrary("/redlaboratory/globalinput/lib/windows/libUnicodeTyper64.dll");
+			} else {
+				loadLibrary("/redlaboratory/globalinput/lib/windows/libUnicodeTyper32.dll");
+			}
+		}
+	}
+	
+	private static String loadLibrary(String name) {
+		try {
+			String fileName = name.substring(name.lastIndexOf('/') + 1);
+			String tmpFolderPath = System.getProperty("java.io.tmpdir");
+			if (tmpFolderPath.charAt(tmpFolderPath.length() - 1) == File.separatorChar) tmpFolderPath = tmpFolderPath.substring(0, tmpFolderPath.lastIndexOf(File.separatorChar));
+			String tmpFilePath = tmpFolderPath + File.separatorChar + fileName;
+			
+			File tmpFile = new File(tmpFilePath);
+			
+			InputStream is = GlobalInput.class.getResourceAsStream(name);
+			FileOutputStream fos = new FileOutputStream(tmpFile);
+			
+			byte buffer[] = new byte[4096];
+			int n;
+			while ((n = is.read(buffer, 0, buffer.length)) > 0) {
+				fos.write(buffer, 0, n);
+			}
+			
+			fos.close();
+			is.close();
+			
+			System.load(tmpFilePath);
+			
+			return tmpFilePath;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+}
---
+++ src/redlaboratory/globalinput/Test.java
@@ -0,0 +1,10 @@
+package redlaboratory.globalinput;
+
+public class Test {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(2000);
+		GlobalInput.typeString("heeeeeeeeeeeeelllllllllllllllo, world! ¾È³ç, ¼¼»ó!");
+	}
+	
+}
