971b8a9e6877fa35ee3742d566672dadd496b515
RedLaboratory <rlj1202@gmail.com>
2016-08-27 23:26:54 +0900

    Combine c project and java project.
    Combined c project as a child project in gradle system.

---
+++ build/resources/jGlobalInputManagerTest/jar/redlaboratory/globalinputmanager/hookEvent/GlobalKeyboardEventHandler.java
@@ -0,0 +1,9 @@
+package redlaboratory.globalinputmanager.hookEvent;
+
+public interface GlobalKeyboardEventHandler {
+	
+	void keyPressed(GlobalKeyboardEvent event);
+	
+	void keyReleased(GlobalKeyboardEvent event);
+	
+}
